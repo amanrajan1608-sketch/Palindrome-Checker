0e94ac5361c77e6091456f79863487b815327667
amanrajan1608 <amanrajan1608@gmail.com>
2026-02-27 15:24:10 +0530

    UC1: Application Entry & Welcome Message

---
+++ PalindromeChecker.java
@@ -0,0 +1,9 @@
+
+
+public class PalindromeChecker{
+    public static void main(String[] args){
+        System.out.println("Welcome to palindrome Checker Manangement App");
+        System.out.println("Version 1.0");
+        System.out.println("System initialized successfully");
+    }
+}
